7d8be19ba6a2a3224e8bd2819f9884d218c5bae6
saizk <sergioaizcorbe@hotmail.com>
2021-05-19 15:10:05 +0200

    Changed politic by politician xd

--- nlp/scraper/models.py
+++ nlp/scraper/models.py
@@ -12,17 +12,17 @@ def init_db(db_uri):
     Model.metadata.bind = engine
     session = orm.scoped_session(orm.sessionmaker(bind=engine))
     Tweet.__table__.create(bind=engine, checkfirst=True)
-    Politic.__table__.create(bind=engine, checkfirst=True)
+    Politician.__table__.create(bind=engine, checkfirst=True)
     Party.__table__.create(bind=engine, checkfirst=True)
     return session, engine
 
 
-def create_politic(db, name, party, twitter):
-    pol = db.query(Politic).filter_by(politic_id=hash(name)).first()
+def create_politician(db, name, party, twitter):
+    pol = db.query(Politician).filter_by(politician_id=hash(name)).first()
     if pol is None:
-        pol = Politic(
+        pol = Politician(
             name=name, party=party,
-            twitter=twitter, politic_id=hash(name)
+            twitter=twitter, politician_id=hash(name)
         )
         db.add(pol)
 
@@ -39,28 +39,28 @@ def create_party(db, party, twitter):
 def save_tweet(db, tweet):
     db_tweet = db.query(Tweet).filter_by(tweet_id=hash(tweet.id)).first()
     if db_tweet is None:
-        politic = get_politic(db, tweet.user.screen_name)
+        politician = get_politicians(db, tweet.user.screen_name)
         db_tweet = Tweet(
-            party=politic.party,
+            party=politician.party,
             text=tweet.full_text,
             retweets=tweet.retweet_count,
             favs=tweet.favorite_count,
             created_at=tweet.created_at,
-            author_id=politic.id,
+            author_id=politician.id,
             tweet_id=hash(tweet.id),
         )
         db.add(db_tweet)
 
 
-def get_politic(db, twitter):
-    politic = db.query(Politic).filter_by(twitter=twitter).first()
-    return politic
+def get_politicians(db, twitter):
+    politician = db.query(Politician).filter_by(twitter=twitter).first()
+    return politician
 
 
 def get_politicians_twitter_dict(db):
     twitters = {
-        user[0]: twitter[0].lower() for user, twitter in zip(db.query(Politic.name),
-                                                             db.query(Politic.twitter)) if twitter[0]
+        user[0]: twitter[0].lower() for user, twitter in zip(db.query(Politician.name),
+                                                             db.query(Politician.twitter)) if twitter[0]
     }
     return twitters
 
@@ -74,18 +74,18 @@ class Tweet(Model):
     party = sq.Column(sq.String(128), nullable=False)
 
     created_at = sq.Column(sq.DateTime, nullable=False)
-    author_id = sq.Column(sq.Integer, sq.ForeignKey('politic.id'), nullable=False)
+    author_id = sq.Column(sq.Integer, sq.ForeignKey('politician.id'), nullable=False)
     tweet_id = sq.Column(sq.Integer, nullable=False)  # hash
 
 
-class Politic(Model):
+class Politician(Model):
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String, nullable=False)
     twitter = sq.Column(sq.String(128))
     party = sq.Column(sq.String(128), nullable=False)
-    politic_id = sq.Column(sq.Integer, nullable=False)  # hash
+    politician_id = sq.Column(sq.Integer, nullable=False)  # hash
     tweets = orm.relationship("Tweet",
-                              backref="politic",
+                              backref="politician",
                               cascade="all, delete-orphan")
 
 
